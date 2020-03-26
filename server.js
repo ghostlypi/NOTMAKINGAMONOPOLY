cb8042302a93e25b6506ea84351338ed7e2830d9
ghostlypi <parth.iyer@gmail.com>
2020-03-26 10:54:56 -0700

    Made server.js web compatable

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,17 +12,14 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   io.emit('chat message', 'a user connected');
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    io.emit('chat message', 'user disconnected');
-    console.log('user disconnected');
-  });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    console.log('message: ' + msg);
+  });
+  socket.on('disconnect', function(){
+    io.emit('chat message', 'user disconnected');
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000')
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
